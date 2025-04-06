8d96831429fd102b7ed9b9fb391f3b22dde3ed98
MoranMishan <mishanmo@gmail.com>
2025-04-06 22:15:06 +0300

    Add src/components/controls.js

---
+++ src/components/controls.js
@@ -0,0 +1,11 @@
+import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js';
+
+export function createControls(camera, renderer) {
+    const controls = new PointerLockControls(camera, renderer.domElement);
+
+    document.addEventListener('click', () => {
+        controls.lock();
+    });
+
+    return controls;
+}
